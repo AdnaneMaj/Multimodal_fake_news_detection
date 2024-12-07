4d9b77bc5ef762c63dbb243bbb15f649d1d79b0f
adnanemajdoub@gmail.com <adnanemajdoub@gmail.com>
2024-12-07 09:52:05 +0100

    Added multimodality

--- src/Enums.py
+++ src/Enums.py
@@ -3,4 +3,5 @@ from enum import Enum
 class BaseEnum(Enum):
 
     DATA_PATH = "./data/"
-    WORD2VEC_MODEL = "src/models/GoogleNews-vectors-negative300.bin"
+    WORD2VEC_MODEL = "src/models/embeddings/GoogleNews-vectors-negative300.bin"
+    YOLO_MODEL = "./src/models/yolo/yolo11n.pt"
